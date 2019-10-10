82404fea4a7bfaf82a7f7e3d7cfa3d061c88322d
Kris Jordan <krisjordan@gmail.com>
2019-10-10 10:44:57 -0400

    Add lec 12 examples

---
+++ src/12-object-oriented-programming/00-simple-method-app.ts
@@ -0,0 +1,19 @@
+import { print } from "introcs";
+
+class Person {
+    name: string = "";
+
+    // TODO: Add a sayHello Method
+
+}
+
+export let main = async () => {
+
+    let aPerson = new Person();
+    aPerson.name = "Alexandria";
+
+    // TODO: call sayHello
+
+};
+
+main();
