a208fdc22a4284dadc2431b76b69219682a621a7
Kris Jordan <krisjordan@gmail.com>
2019-10-31 11:55:22 -0400

    Fix first example demo

--- src/16-recursion-recursion/00-includes-app.ts
+++ src/16-recursion-recursion/00-includes-app.ts
@@ -11,13 +11,7 @@ export let main = async () => {
 
 // TODO: Implement the includes Function
 let includes = (list: Node<string>, search: string): boolean => {
-    if (list === null) {
-        return false;
-    } else if (first(list) === search) {
-        return true;
-    } else {
-        return includes(rest(list), search);
-    }
+    return false;
 };
 
 main();
