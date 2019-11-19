23df565d85b4c8bc3334189234a17d53ccf20362
Kris Jordan <krisjordan@gmail.com>
2019-11-19 12:52:15 -0500

    Add example of f/m/r with weather stats

--- src/20-higher-order/00-weather-redux-app.ts
+++ src/20-higher-order/00-weather-redux-app.ts
@@ -12,26 +12,26 @@ let main = async () => {
     let data = await csvToArray("Select Data File", WeatherRow);
 
     /* TODO: filter data by days where precipitation > 0 */
-    let daysWithPrecipitation: WeatherRow[] = [];
+    let daysWithRain: WeatherRow[] = [];
 
     /* TODO: assign the number of days with precipitation to variable */
-    let countPrecipitation = 0;
+    let countRain = 0;
 
     /* TODO: map days with precipitation to array of precipitation amount */
-    let precipitation: number[] = [];
+    let rain: number[] = [];
 
     /* TODO: reduce the precipitation amounts via a sum */
-    let sumPrecipitation = 0;
+    let sumRain = 0;
 
     /* TODO: reduce the precipitation amounts via a max */
-    let maxPrecipitation = 0;
+    let maxRain = 0;
 
-    print("Days with Precipitation:" + countPrecipitation);
-    print("Total Precipitation:" + sumPrecipitation);
-    print("Max Precipitation:" + maxPrecipitation);
+    print("Days with Rain:" + countRain);
+    print("Total Rain:" + sumRain);
+    print("Max Rain:" + maxRain);
 
     /* TODO: Print the days where the max precipitation was recorded */
-    print("Dates with record precipitation:");
+    print("Dates with record rain:");
 
 };
 
